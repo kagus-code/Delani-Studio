8e9a8d65f48ed7fcec5ad2f931be70232caa9340
Eston Kagwima <ekagwima745@gmail.com>
2021-02-27 03:25:27 +0300

    making icons  clickable individually

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,15 @@
 $(document).ready(function() {
-  $(".clickable").click(function() {
-    $(".click-show").toggle();
-    $(".click-hidden").toggle();
+  $(".clickable1").click(function() {
+    $(".click-show1").toggle();
+    $(".click-hidden1").toggle();
   });
+  $(".clickable2").click(function() {
+    $(".click-show2").toggle();
+    $(".click-hidden2").toggle();
+  });
+  $(".clickable3").click(function() {
+    $(".click-show3").toggle();
+    $(".click-hidden3").toggle();
+  });
+  
 });
